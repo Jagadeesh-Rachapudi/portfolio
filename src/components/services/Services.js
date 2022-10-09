c899fc5fea857babb1eea2b88505eafcbc881adb
maruthi <Maruthi@noodle.digital>
2022-10-09 12:30:47 +0530

    finished monbile version

--- src/components/services/Services.js
+++ src/components/services/Services.js
@@ -1,40 +1,43 @@
+/* eslint-disable jsx-a11y/alt-text */
 import React from "react";
 import "./Services.css";
+import Container from "react-bootstrap/Container";
+import Row from "react-bootstrap/Row";
+import Col from "react-bootstrap/Col";
 function Services() {
   return (
-    <div className="totlabody">
-      <div className="servicesintro">SERVICES</div>
-      <div className="shedding">Check My Services</div>
-      <div className="sgetall">Explore All Services</div>
-      <div className="scontainer">
-        <div className="spic1">
-          <img src="https://firebasestorage.googleapis.com/v0/b/portfolio-39221.appspot.com/o/Portfolio-Images%2FFrame%208.png?alt=media&token=1532a290-b44a-45d4-87eb-b5e471a0c5a7"></img>
-          <div className="spic1hedding">Landing Page Design</div>
-          <div className="spic1content">
-            is simply dummy text of the printing and typesetting industry. Lorem
-            Ipsum has been the industry's standard dummy text ever since the
-            1500s
-          </div>
-        </div>
-        <div className="spic2">
-          <img src="https://firebasestorage.googleapis.com/v0/b/portfolio-39221.appspot.com/o/Portfolio-Images%2FFrame%209.png?alt=media&token=1b380bb2-7db4-4401-929d-0328c9b5f071"></img>
-          <div className="spic2hedding">Mobile App Design</div>
-          <div className="spic2content">
-            is simply dummy text of the printing and typesetting industry. Lorem
-            Ipsum has been the industry's standard dummy text ever since the
-            1500s
-          </div>
-        </div>
-        <div className="spic3">
-          <img src="https://firebasestorage.googleapis.com/v0/b/portfolio-39221.appspot.com/o/Portfolio-Images%2FFrame%2010.png?alt=media&token=87142e29-96d0-4c00-8969-f8bd1fbc9388"></img>
-          <div className="spic3hedding">Branding</div>
-          <div className="spic3content">
-            is simply dummy text of the printing and typesetting industry. Lorem
-            Ipsum has been the industry's standard dummy text ever since the
-            1500s
-          </div>
-        </div>
-      </div>
+    <div>
+      <Container>
+        <Row className="justify-content-md-center">
+          <Col class="d-flex flex-column">
+            <img src="https://firebasestorage.googleapis.com/v0/b/portfolio-39221.appspot.com/o/Portfolio-Images%2FFrame%208.png?alt=media&token=1532a290-b44a-45d4-87eb-b5e471a0c5a7" />
+            <div className="shedding">Landing page design</div>
+            <div className="scontent">
+              is simply dummy text of the printing and typesetting industry.
+              Lorem Ipsum has been the industry's standard dummy text ever since
+              the 1500s
+            </div>
+          </Col>
+          <Col class="d-flex flex-column">
+            <img src="https://firebasestorage.googleapis.com/v0/b/portfolio-39221.appspot.com/o/Portfolio-Images%2FFrame%209.png?alt=media&token=1b380bb2-7db4-4401-929d-0328c9b5f071" />
+            <div className="shedding">Landing page design</div>
+            <div className="scontent">
+              is simply dummy text of the printing and typesetting industry.
+              Lorem Ipsum has been the industry's standard dummy text ever since
+              the 1500s
+            </div>
+          </Col>
+          <Col class="d-flex flex-column">
+            <img src="https://firebasestorage.googleapis.com/v0/b/portfolio-39221.appspot.com/o/Portfolio-Images%2FFrame%2010.png?alt=media&token=87142e29-96d0-4c00-8969-f8bd1fbc9388" />
+            <div className="shedding">Landing page design</div>
+            <div className="scontent">
+              is simply dummy text of the printing and typesetting industry.
+              Lorem Ipsum has been the industry's standard dummy text ever since
+              the 1500s
+            </div>
+          </Col>
+        </Row>
+      </Container>
     </div>
   );
 }
