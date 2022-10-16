7382271069a8541a799f38b41d0e390a839b1cdb
Maruthi <Maruthi@noodle.digital>
2022-10-16 23:36:02 +0530

    Testimonial

--- src/components/testimonials/Testimonial.js
+++ src/components/testimonials/Testimonial.js
@@ -1,56 +1,40 @@
 /* eslint-disable jsx-a11y/alt-text */
 import Button from "react-bootstrap/Button";
 import React from "react";
-import "./Testiomonial.css";
+import "./Testiomonial.scss";
+import { RiDoubleQuotesL, RiDoubleQuotesR } from "react-icons/ri";
 
 function Testimonial() {
   return (
-    <div className="testibody" id='tesimonials'>
-      <div className="testiintro">TESTIMONIALS</div>
-      <div className="testihedding">What People Says</div>
-      <div className="testiroseboxcontainer">
-        <div className="testirosebox"></div>
-
-        <div className="testimage">
-          <img src="https://firebasestorage.googleapis.com/v0/b/portfolio-39221.appspot.com/o/Portfolio-Images%2Ftesti%20image.png?alt=media&token=e2634f66-8774-4a43-a8c6-32ba1007d64d" />
-        </div>
-        <div className="testicardcontainer">
-          <div className="testicard">
-            <img
-              className="up"
-              src="https://firebasestorage.googleapis.com/v0/b/portfolio-39221.appspot.com/o/Portfolio-Images%2Fdown.png?alt=media&token=7b4178e2-6264-4c13-b084-85ef446a6fb7"
-            ></img>
-
-            <div className="testicardcontent">
-              is simply dummy text of the printing and typesetting industry.
-              Lorem Ipsum has been the industry's standard dummy text ever since
-              the 1500s
-              <div className="peoplename">Karmani</div>
-              <div className="testirole">FOUNDER AND CEO OF KARMANI</div>
-              <div className="downcontainer">
-                <img
-                  className="down"
-                  src="https://firebasestorage.googleapis.com/v0/b/portfolio-39221.appspot.com/o/Portfolio-Images%2Fup.png?alt=media&token=c6ed4476-6272-45dc-9508-587d3e02fa57"
-                ></img>
-              </div>
-            </div>
-          </div>
-        </div>
-        <img
-          className="testiRectangles"
-          src="https://firebasestorage.googleapis.com/v0/b/portfolio-39221.appspot.com/o/Portfolio-Images%2FFrame%2017.png?alt=media&token=76b7aff0-9901-41dc-8e63-c541eb56f2c6"
-        ></img>
-      </div>
-      <div className="testibuttons">
-        <Button variant="warning" size="lg">
-          {"←"}
-        </Button>
-        <Button variant="warning" size="lg">
-          {"→"}
-        </Button>
-      </div>
+    <div className="testimonial">
+      <h6>Testimonials</h6>
+      <h2>What People Says</h2>
+      <TestimonialCard />
     </div>
   );
 }
 
+function TestimonialCard() {
+  return (
+    <div className="testimonial-card">
+      <img
+        className="avaatar"
+        src="https://i.pinimg.com/474x/1f/53/45/1f5345c69adb4511f213ea826024ed67.jpg"
+      />
+      <div className="content">
+        <RiDoubleQuotesL className="left" />
+        <div className="sub-content">
+          <p>
+            is simply dummy text of the printing and typesetting industry. Lorem
+            Ipsum has been the industry's standard dummy text ever since the
+            1500s
+          </p>
+          <p className="author"> Karmani</p>
+          <p className="designation">FOUNDER AND CEO OF KARMANI</p>
+        </div>
+        <RiDoubleQuotesR className="right" />
+      </div>
+    </div>
+  );
+}
 export default Testimonial;
